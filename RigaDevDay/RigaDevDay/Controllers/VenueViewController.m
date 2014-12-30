cfea8f180db7e8a5e097725dc28f3cbc80294caf
TheTaier <oneander@gmail.com>
2014-12-30 12:23:51 +0200

    Add Venue Pins and navigation

--- RigaDevDay/RigaDevDay/Controllers/VenueViewController.m
+++ RigaDevDay/RigaDevDay/Controllers/VenueViewController.m
@@ -12,7 +12,9 @@
 #import "MKMapView+ZoomLevel.h"
 #import "SWRevealViewController.h"
 
-@interface VenueViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+@interface VenueViewController () <MKMapViewDelegate, CLLocationManagerDelegate> {
+    BOOL _updateStartLocation;
+}
 @property (strong, nonatomic) CLLocationManager *locationManager;
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *buttonMenu;
@@ -32,13 +34,14 @@
     self.mapView.delegate = self;
     self.mapView.showsUserLocation = YES;
     
-
+    _updateStartLocation = NO;
     self.locationManager = [[CLLocationManager alloc]init];
     self.locationManager.delegate = self;
     if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
         [self.locationManager requestWhenInUseAuthorization];
     }
     [self.locationManager startUpdatingLocation];
+    [self setupPins];
     // Do any additional setup after loading the view.
 }
 
@@ -59,13 +62,76 @@
 
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
 {
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 800, 800);
+    if(_updateStartLocation) return;
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 1500, 1500);
     [self.mapView setRegion:[self.mapView regionThatFits:region] animated:YES];
+    _updateStartLocation = YES;
 }
 
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray *)locations {
     
+}
+
+- (void)setupPins {
+    NSMutableArray* annotations=[[NSMutableArray alloc] init];
+    
+    CLLocationCoordinate2D rigaPlazaCoord;
+    rigaPlazaCoord.latitude = 56.9253666;
+    rigaPlazaCoord.longitude = 24.102882;
+    
+    CLLocationCoordinate2D islandHotelCoord;
+    islandHotelCoord.latitude = 56.9511414;
+    islandHotelCoord.longitude = 24.0841795;
+    
+    MKPointAnnotation *rigaPlazaAnnotation = [[MKPointAnnotation alloc] init];
+    rigaPlazaAnnotation.coordinate = rigaPlazaCoord;
+    rigaPlazaAnnotation.title = @"Riga Plaza";
+    rigaPlazaAnnotation.subtitle = @"Conference Place";
+    [annotations addObject:rigaPlazaAnnotation];
+    
+    
+    MKPointAnnotation *islandHotelAnotation = [[MKPointAnnotation alloc] init];
+    islandHotelAnotation.coordinate = islandHotelCoord;
+    islandHotelAnotation.title = @"Islande Hotel";
+    islandHotelAnotation.subtitle = @"Workshop Place";
+    [annotations addObject:islandHotelAnotation];
+
+    [self.mapView addAnnotations:annotations];
+}
+
+-(void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
+    
+}
+
+
+- (MKAnnotationView *)mapView:(MKMapView *)sender viewForAnnotation:(id <MKAnnotation >)annotation
+{
+    
+    if (![annotation subtitle]) return nil;
+    static NSString *reuseId = @"StandardPin";
+    
+    MKPinAnnotationView *aView = (MKPinAnnotationView *)[sender
+                                                         dequeueReusableAnnotationViewWithIdentifier:reuseId];
+    if (aView == nil)
+    {
+        aView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation
+                                                 reuseIdentifier:reuseId];
+        aView.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        aView.canShowCallout = YES;
+    }
+    
+    aView.annotation = annotation;
+    
+    return aView;   
+}
+
+- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view
+calloutAccessoryControlTapped:(UIControl *)control
+{
+    NSString* addr = [NSString stringWithFormat:@"http://maps.apple.com/maps?daddr=%1.6f,%1.6f&saddr=Posizione attuale", view.annotation.coordinate.latitude,view.annotation.coordinate.longitude];
+    NSURL* url = [[NSURL alloc] initWithString:[addr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    [[UIApplication sharedApplication] openURL:url];
 }
 
 @end
